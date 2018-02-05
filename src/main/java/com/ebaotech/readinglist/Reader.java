cabaee5f8014f52b276707a22a6bd8194ef07e9a
herbert.tang <dondavd@163.com>
2018-02-05 09:27:22 +0800

    first commit

---
+++ src/main/java/com/ebaotech/readinglist/Reader.java
@@ -0,0 +1,75 @@
+package com.ebaotech.readinglist;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+public class Reader implements UserDetails{
+
+  private static final long serialVersionUID = 3876656020943203204L;
+
+  @Id
+  private String username;
+  private String fullname;
+  private String password;
+  
+  public String getFullname() {
+    return fullname;
+  }
+
+  public void setFullname(String fullname) {
+    this.fullname = fullname;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  @Override
+  public String getPassword() {
+    return null;
+  }
+  
+  @Override
+  public String getUsername() {
+    return null;
+  }
+  
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return Arrays.asList(new SimpleGrantedAuthority("READER")); //grant 'READER' authority
+  }
+
+
+  @Override
+  public boolean isAccountNonExpired() { // account won't be expired
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+  
+}
